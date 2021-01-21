0f2e2ffbd4275c78c712af7b0def19b1e1341184
Asemxu <hijo72@hotmail.com>
2021-01-21 14:24:05 -0500

    Subiendo la prueba

--- src/Components/Tienda/ListProductsCarrito.js
+++ src/Components/Tienda/ListProductsCarrito.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ProductCarrito from './ProductCarrito';
 export default class ListProductsCarrito extends React.Component{
-    
     render(){
+        let total = 0;
         return(
             <React.Fragment>
                 <ul className="lista_products_carrito">
                     {this.props.products.map((product) => {
+                        let price = parseFloat(product.price.amount);
+                        total+=price;
                         return(
                             <li key={product.id}>
                                 <ProductCarrito
@@ -17,6 +19,10 @@ export default class ListProductsCarrito extends React.Component{
                             </li>                      
                         )
                     })}
+                    <hr/>
+                    <div clasName="total_costo">
+                        <h2>Total : {total.toFixed(2)}</h2>
+                    </div>
                 </ul>
             </React.Fragment>
         )
